9c9ca0aceea79ef01c0fadd3df38e339eee62c7d
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2023-05-05 19:03:43 +0330

    remove previous project

--- app/src/reducers/headerReducer.js
+++
@@ -1,8 +0,0 @@
-export default (state = { IsHeader: true }, action) => {
-  switch (action.type) {
-    case 'HEADER_HIDDEN':
-      return { IsHeader: action.payload };
-    default:
-      return state;
-  }
-};
