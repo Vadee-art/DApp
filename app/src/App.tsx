b501b4e02a2851816cb9dd8e3171971c642cb804
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2023-05-06 17:23:36 +0330

    add react router to project

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,8 +1,17 @@
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { AppProvider } from "./providers/app";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>Hello world!</div>,
+  },
+]);
 
 function App() {
   return (
     <AppProvider>
+      <RouterProvider router={router} />
     </AppProvider>
   );
 }
