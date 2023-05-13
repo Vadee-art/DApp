13898179e4e88cb7eadea2a61fb913560da08fdb
Mohammad SalehiVaziri <mohammadsalehivaziri@gmail.com>
2023-05-13 19:01:04 +0330

    add storage util

---
+++ app/src/utils/storage.ts
@@ -0,0 +1,18 @@
+import { AuthUser } from "@/features/auth";
+
+const storagePrefix = 'vadee_';
+
+const storage = {
+  // User
+  getUser: () => {
+    return JSON.parse(window.localStorage.getItem(`${storagePrefix}user`) as string);
+  },
+  setUser: (user: AuthUser) => {
+    window.localStorage.setItem(`${storagePrefix}user`, JSON.stringify(user));
+  },
+  clearUser: () => {
+    window.localStorage.removeItem(`${storagePrefix}user`);
+  },
+};
+
+export default storage;
