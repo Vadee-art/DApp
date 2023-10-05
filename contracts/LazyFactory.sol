5dd786535c5fe96c34bbd21062102b7b340a9641
Arash Fatahzade <fatahzade@gmail.com>
2023-10-05 12:29:35 +0330

    Modified contracts

--- contracts/LazyFactory.sol
+++ contracts/LazyFactory.sol
@@ -6,16 +6,17 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Royalty.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "hardhat/console.sol";
 
 contract LazyFactory is
-    ERC721URIStorage,
     EIP712,
+    ERC721URIStorage,
+    ERC721Royalty,
     AccessControl,
     ReentrancyGuard
 {
-    address payable theMarketPlace;
     address payable theArtist;
     string private constant SIGNING_DOMAIN_NAME = "VADEE";
     string private constant SIGNING_DOMAIN_VERSION = "1";
@@ -24,6 +25,9 @@ contract LazyFactory is
         keccak256(
             "Voucher(string title,uint256 artworkId,string editionNumber,string edition,uint256 priceWei,string priceDollar,string tokenUri,string content)"
         );
+
+    uint256 vadeeFee;
+    address payable vadeeAddress;
 
     struct Voucher {
         string title;
@@ -42,37 +46,42 @@ contract LazyFactory is
     mapping(address => uint256) private balanceByAddress;
 
     constructor(
-        address payable marketplaceAddress,
         string memory name,
         string memory symbol,
-        address payable artist
+        address payable artist,
+        uint256 _vadeeFee,
+        address payable _vadeeAddress,
+        uint96 royaltyFee
     ) ERC721(name, symbol) EIP712(SIGNING_DOMAIN_NAME, SIGNING_DOMAIN_VERSION) {
+        require(_vadeeFee < 100, "vadeeFee too high");
+        require(_vadeeFee >= 0, "vadeeFee should be positive");
+
         _setupRole(SIGNER_ROLE, artist);
-        theMarketPlace = marketplaceAddress;
         theArtist = artist;
+        vadeeFee = _vadeeFee;
+        vadeeAddress = _vadeeAddress;
+        _setDefaultRoyalty(artist, royaltyFee);
     }
 
     function redeem(
         address buyer,
-        Voucher calldata voucher,
-        uint256 vadeeFee
+        Voucher calldata voucher
     ) public payable nonReentrant {
         address artist = _verify(voucher);
 
         require(msg.value == voucher.priceWei, "Enter the correct price");
-        require(artist != buyer, "You can not purchase your own token");
+        require(artist != buyer, "artist cannot purchase");
         require(hasRole(SIGNER_ROLE, artist), "Invalid Signature");
+
+        uint256 amount = msg.value;
+        payable(artist).transfer(amount - (100 - vadeeFee));
+        payable(vadeeAddress).transfer(amount * vadeeFee);
 
         _mint(artist, voucher.artworkId);
         _setTokenURI(voucher.artworkId, voucher.tokenUri);
-        setApprovalForAll(theMarketPlace, true); // sender approves Market Place to transfer tokens
 
         // transfer the token to the buyer
         _transfer(artist, buyer, voucher.artworkId);
-
-        uint256 amount = msg.value;
-        payable(artist).transfer(amount - vadeeFee);
-        payable(theMarketPlace).transfer(vadeeFee);
 
         emit RedeemedAndMinted(voucher.artworkId);
     }
@@ -113,15 +122,30 @@ contract LazyFactory is
         return id;
     }
 
-    function supportsInterface(bytes4 interfaceId)
+    function supportsInterface(
+        bytes4 interfaceId
+    )
         public
         view
         virtual
-        override(AccessControl, ERC721)
+        override(AccessControl, ERC721, ERC721Royalty)
         returns (bool)
     {
         return
             ERC721.supportsInterface(interfaceId) ||
-            AccessControl.supportsInterface(interfaceId);
+            AccessControl.supportsInterface(interfaceId) ||
+            ERC721Royalty.supportsInterface(interfaceId);
+    }
+
+    function tokenURI(
+        uint256 tokenId
+    ) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+        return super.tokenURI(tokenId);
+    }
+
+    function _burn(
+        uint256 tokenId
+    ) internal override(ERC721URIStorage, ERC721Royalty) {
+        super._burn(tokenId);
     }
 }
